384e44519d3a96c3267608a801afb09a4ea3b0b3
Foster BI Services <fosterbiservices@gmail.com>
2025-03-17 07:31:04 -0400

    Removed Reference Files

--- SQL/Reference Queries/AddColumns.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE NADCS_DW.dbo.BPOScorecardVoiceGoals
-ADD		QAPercentNum decimal(8,4)
-		,QAPercentDenom decimal(8,4);
--- SQL/Reference Queries/zRenameColumns.sql
+++
@@ -1,8 +0,0 @@
-USE [NADCS_DW]
-GO 
-
-sp_rename 'BPOScorecardVoiceGoals.QANum', 'QAMCOENum';
-
-USE [NADCS_DW]
-GO 
-sp_rename 'BPOScorecardVoiceGoals.QADenom', 'QAMCOEDenom';
