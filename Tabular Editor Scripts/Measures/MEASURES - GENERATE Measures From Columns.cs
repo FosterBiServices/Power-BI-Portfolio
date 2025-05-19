fa385b7e1c4ff80b4aac3b6fa12c76ad78fcfc78
Foster BI Services <fosterbiservices@gmail.com>
2025-05-19 09:52:39 -0400

    Update MEASURES - GENERATE Measures From Columns.cs
    Added display folder
    
    Signed-off-by: Foster BI Services <fosterbiservices@gmail.com>

--- Tabular Editor Scripts/Measures/MEASURES - GENERATE Measures From Columns.cs
+++ Tabular Editor Scripts/Measures/MEASURES - GENERATE Measures From Columns.cs
@@ -1,45 +1,42 @@
+// Get reference to the __Measures table
+var measuresTable = Model.Tables.FirstOrDefault(t => t.Name == "__Measures");
+
+if(measuresTable == null) {
+    throw new Exception("Table '__Measures' not found.");
+}
+
 foreach(var c in Selected.Columns) {
     var oldName = c.Name;
     var newName = new System.Text.StringBuilder();
+
     for(int i = 0; i < oldName.Length; i++) {
-        // First letter should always be capitalized:
-        if(i == 0) newName.Append(Char.ToUpper(oldName[i]));
-
-        // A sequence of two uppercase letters followed by a lowercase letter should have a space inserted
-        // after the first letter:
-        else if(i + 2 < oldName.Length && char.IsLower(oldName[i + 2]) && char.IsUpper(oldName[i + 1]) && char.IsUpper(oldName[i]))
-        {
+        if(i == 0) {
+            newName.Append(Char.ToUpper(oldName[i]));
+        }
+        else if(i + 2 < oldName.Length && Char.IsLower(oldName[i + 2]) && Char.IsUpper(oldName[i + 1]) && Char.IsUpper(oldName[i])) {
             newName.Append(oldName[i]);
             newName.Append(" ");
         }
-
-        // All other sequences of a lowercase letter followed by an uppercase letter, should have a space
-        // inserted after the first letter:
-        else if(i + 1 < oldName.Length && char.IsLower(oldName[i]) && char.IsUpper(oldName[i+1]))
-        {
+        else if(i + 1 < oldName.Length && Char.IsLower(oldName[i]) && Char.IsUpper(oldName[i+1])) {
             newName.Append(oldName[i]);
             newName.Append(" ");
         }
-        else
-        {
+        else {
             newName.Append(oldName[i]);
         }
     }
-{
-    var newMeasure = c.Table.AddMeasure(
-        newName.ToString(),                    // Name
-        "SUM(" + c.DaxObjectFullName + ")",    // DAX expression
-        c.DisplayFolder                        // Display Folder
+
+    var derivedTableName = c.Table.Name;
+
+    var newMeasure = measuresTable.AddMeasure(
+        newName.ToString(),                      // Name
+        "SUM(" + c.DaxObjectFullName + ")",      // DAX expression
+        derivedTableName                         // Display Folder named after original table
     );
-    
-    // Set the format string on the new measure:
+
     newMeasure.FormatString = "#,##";
 
-    // Provide some documentation:
     newMeasure.Description = "This measure is the sum of column " + c.DaxObjectFullName;
 
-    // Hide the base column:
     c.IsHidden = true;
-}
-    
 }
